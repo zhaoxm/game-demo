4d39afae2ac14263c1b0c97b3c4dd4db4a5fc000
zhaoxm <hizhaoxm@gmail.com>
2026-06-25 11:54:39 +0800

    feat

--- main.gd
+++ main.gd
@@ -20,6 +20,8 @@ func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 	
 func new_game():
 	score = 0
@@ -28,6 +30,7 @@ func new_game():
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
 	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 
 func _on_mob_timer_timeout() -> void:
